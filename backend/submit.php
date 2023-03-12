404ec38e9a60496141f9ef812ac7576e75c2c943
Staninna <26458805+Staninna@users.noreply.github.com>
2023-03-12 22:19:04 +0100

    Fixed some comments other things

--- backend/submit.php
+++ backend/submit.php
@@ -1,6 +1,6 @@
 <?php
 
-// Start the session
+// Start session
 session_start();
 
 // Get data
@@ -8,24 +8,25 @@ $winner = $_POST['winner'];
 $pokemon_1 = $_SESSION['pokemon_1'];
 $pokemon_2 = $_SESSION['pokemon_2'];
 
-// Check data if something is wrong redirect to index
 if (
     !isset($_POST['form_id']) || !isset($_POST['winner']) ||     // Check if all data is set
     $_POST['form_id'] !== $_SESSION['form_id'] ||                // Check if form id is valid
-    $winner !== $pokemon_1['id'] && $winner !== $pokemon_2['id'] // Check if winner is valid
+    $winner != $pokemon_1['id'] && $winner != $pokemon_2['id']   // Check if winner is valid
 ) {
+    // Destroy session
     session_destroy();
-    header('Location: ../index.php');
+
+    // Redirect to index
+    header('Location: ../index.php?error=invalid_data');
     exit();
 }
-
 
 // Get winner and loser
 $winner = $winner === $pokemon_1['id'] ? $pokemon_1 : $pokemon_2;
 $loser = $winner === $pokemon_1 ? $pokemon_2 : $pokemon_1;
 
 // Connect to database
-include_once 'conn.php';
+include_once('conn.php');
 
 // Update winner
 $query = "UPDATE pokemon SET voted_for = voted_for + 1 WHERE id = :id";
