2058eb66563b2109f3bff67cd3b3f4aa9f46978a
Staninna <26458805+Staninna@users.noreply.github.com>
2023-03-13 10:21:43 +0100

    Converted to elo rating

--- results.php
+++ results.php
@@ -4,10 +4,9 @@
 require_once('backend/conn.php');
 
 // Get all pokemon
-$query = "SELECT id, name, voted_for, voted_against,
-          (voted_for * 100 / (voted_for + voted_against)) AS positive_votes_percentage
+$query = "SELECT id, name, elo_rating
           FROM pokemon
-          ORDER BY positive_votes_percentage DESC, total_votes DESC";
+          ORDER BY elo_rating DESC, total_votes DESC";
 $stmt = $conn->prepare($query);
 $stmt->execute();
 $pokemons = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -30,17 +29,13 @@ $pokemons = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <tr>
             <th>Sprite</th>
             <th>Pokemon</th>
-            <th>Positive votes</th>
-            <th>Negative votes</th>
-            <th>Positive votes percentage</th>
+            <th>Elo Rating</th>
         </tr>
         <?php foreach ($pokemons as $pokemon) { ?>
             <tr>
                 <td><img src="img/mon/<?php echo $pokemon['id']; ?>.png" alt="<?php echo $pokemon['name']; ?>" class="pokemon-sprite" /></td>
                 <td><?php echo $pokemon['name']; ?></td>
-                <td><?php echo $pokemon['voted_for']; ?></td>
-                <td><?php echo $pokemon['voted_against']; ?></td>
-                <td><?php echo $pokemon['positive_votes_percentage']; ?></td>
+                <td><?php echo $pokemon['elo_rating']; ?></td>
             </tr>
         <?php } ?>
     </table>
