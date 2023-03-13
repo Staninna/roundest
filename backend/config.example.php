15b8693e9168f6dc31e13cc4102006194bf7e15c
Staninna <26458805+Staninna@users.noreply.github.com>
2023-03-13 17:22:26 +0100

    Indented to be more readable

--- backend/config.example.php
+++ backend/config.example.php
@@ -8,8 +8,8 @@ $dbPass   = '';
 $base_url = 'http://localhost/';
 
 // Elo rating system
-$score_win = 1;
+$score_win  = 1;
 $score_lose = 0;
 $multiplier = 32;
-$devider = 400;
-$power = 10;
+$devider    = 400;
+$power      = 10;
