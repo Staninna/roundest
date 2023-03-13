bb1e8ad02d37fa60fcd491494e5cb399613c0f2d
Staninna <26458805+Staninna@users.noreply.github.com>
2023-03-13 12:03:50 +0100

    Added elo rating system values

--- backend/config.example.php
+++ backend/config.example.php
@@ -1,7 +1,15 @@
 <?php
 
+// Database
 $dbHost   = 'localhost';
 $dbName   = 'pokemon';
 $dbUser   = 'root';
 $dbPass   = '';
 $base_url = 'http://localhost/';
+
+// Elo rating system
+$score_win = 1;
+$score_lose = 0;
+$multiplier = 32;
+$devider = 400;
+$power = 10;
